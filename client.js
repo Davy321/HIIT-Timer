d11c394dc90867425ffc44224948aac57896a409
Davy321 <56622066+Davy321@users.noreply.github.com>
2021-05-12 14:41:04 -0700

    Update client.js

--- client.js
+++ client.js
@@ -5,14 +5,15 @@ if ("serviceWorker" in navigator) {
 let select = false;
 let TOption = 2; // 0 for premade, 1 for custom
 let level = 0;
+let AdvOption = false;
 
 let warmupTime = 0; // time in seconds
 let highTime = 0; // high intensity training time in seconds
 let lowTime = 0; // low intensity training time in seconds
-let sets = 0; // amounts of sets
+let sets = 0; // amounts of high intensity training sets
 let currentSet = 0;
 let currentIntensity = 0; // 0 for low, 1 for high
-let temp = 0;
+let advancedArray = [];
 
 function selectTimerType(x) {
 	TOption = (x == "premade") ? 0 : 1;
@@ -37,7 +38,8 @@ function advanceTimer() {
 		document.getElementById("error").style.display = "none";
 		
 		document.getElementById("customInputParameters").style.display = "block";
-		document.getElementById("title2").innerHTML = "Enter Timer Parameters:";
+		document.getElementById("advancedSettings").style.display = "block";
+		document.getElementById("title2").innerHTML = "Basic Timer Options:";
 		document.getElementById("btn").innerHTML = "Start Timer";
 		level++;
 		
@@ -47,9 +49,10 @@ function advanceTimer() {
 			
 		}//else
 	} else if (level == 1) {
-		sets = (Number(document.getElementById("setAmount").value) * 2) - 1;
+		setTimerParameters();
     
 		document.getElementById("btn").innerHTML = "Skip";
+		document.getElementById("advancedSettings").style.display = "none";
 		document.getElementById("customInputParameters").style.display = "none";
 		document.getElementById("title2").style.display = "none";
 		
@@ -60,7 +63,7 @@ function advanceTimer() {
 		document.getElementById("actualTimer").style.display = "block";
 		setTimeout(startInterval, warmupTime * 1000);
 		document.body.style.backgroundColor = "#a9dea0";
-		startTimer(warmupTime - 1);
+		startTimer(warmupTime);
 		currentIntensity = 1;
 		
 		level++;
@@ -68,7 +71,23 @@ function advanceTimer() {
     
 	}
 	
-}
+}//advanceTimer
+
+function switchSettings() {
+  if (!AdvOption) {
+	  AdvOption = true;
+	  document.getElementById("customInputParameters").style.display = "none";
+	  document.getElementById("advancedInputParameters").style.display = "block";
+	  document.getElementById("advancedSettings").innerHTML = "Basic Options";
+	  document.getElementById("title2").innerHTML = "Advanced Timer Options:";
+  } else {
+	  AdvOption = false;
+	  document.getElementById("customInputParameters").style.display = "block";
+	  document.getElementById("advancedInputParameters").style.display = "none";
+	  document.getElementById("advancedSettings").innerHTML = "Advanced Options";
+	  document.getElementById("title2").innerHTML = "Basic Timer Options:";
+  }
+}//switchSettings
 
 function startInterval() {
 	if (currentSet >= sets) {
@@ -85,7 +104,7 @@ function startInterval() {
 			currentSet++;
 			startInterval();
 		}, highTime * 1000);
-		startTimer(highTime - 1);
+		startTimer(highTime);
 	} else {
 		document.body.style.backgroundColor = "#f5f2b5";
 		document.getElementById("timerTitle").innerHTML = "Low Intensity";
@@ -94,7 +113,7 @@ function startInterval() {
 			currentSet++;
 			startInterval();
 		}, lowTime * 1000);
-		startTimer(lowTime - 1);
+		startTimer(lowTime);
 	}
 }//startInterval
 
@@ -102,19 +121,16 @@ function startCooldown() {
 	document.body.style.backgroundColor = "#a9dea0";
 	document.getElementById("timerTitle").innerHTML = "Cooldown";
 	setTimeout(function () {
+      document.body.style.backgroundColor = "#d9eaf0";
 			document.getElementById("timerTitle").innerHTML = "You did it,";
 			document.getElementById("timer").innerHTML = "Congrats!";
 			document.getElementById("btn").innerHTML = "Restart";
 		}, warmupTime * 1000);
-		startTimer(warmupTime - 1);
+		startTimer(warmupTime);
 }//startCooldown
 
-let warmupSlider = document.getElementById("warmup");
-let sliderOutput = document.getElementById("warmupValue");
-sliderOutput.innerHTML = warmupSlider.value / 2; // Display the default slider value
-warmupTime = warmupSlider.value * 30;
-
 function startTimer(duration) {
+	duration -= 1;
     let timer = duration, minutes, seconds;
 	let temp = duration + 1;
 	
@@ -139,31 +155,62 @@ function startTimer(duration) {
     }, 1000);
 }//startTimer
 
+function setTimerParameters() {
+	if (!AdvOption) {
+		warmupTime = warmupSlider.value * 30;
+		highTime = highSlider.value * 5;
+		lowTime = lowSlider.value * 5;
+		sets = (Number(document.getElementById("setAmount").value) * 2) - 1;
+	} else {
+		
+	}
+}//getTimerParameters
+
+function createNewInterval() {
+	if (advancedArray.length == 1) {
+		let x = document.getElementById("advancedInputParameters").clientHeight + 200;
+		document.getElementById("advancedInputParameters").style.height = x + "px";
+	}
+	
+	let elem = document.createElement("DIV");
+	elem.innerHTML = "<label>Name: </label>" +
+	"<input placeholder=\"e.g. Warmup\" type=\"text\" value\"sus\">" +
+	"<br><label>Duration in seconds: </label>" +
+	"<input placeholder=\"\" type=\"text\" value\"sus\">" +
+	"<br><label>Interval Type: </label>" +
+	"<select><option value=\"Warmup\">Warmup/Cooldown</option>" + 
+	"<option value=\"High\">High Intensity</option>" +
+	"<option value=\"Low\">Low Intensity</option></select>"
+	;
+	document.getElementById("advancedInputParameters").appendChild(elem);
+	elem.className = "advancedContainerInterval";
+	advancedArray.push(elem);
+	
+}
+
+let warmupSlider = document.getElementById("warmup");
+let sliderOutput = document.getElementById("warmupValue");
+sliderOutput.innerHTML = warmupSlider.value / 2; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 warmupSlider.oninput = function() {
   sliderOutput.innerHTML = this.value / 2;
-  warmupTime = this.value * 30;
 };
 
 let highSlider = document.getElementById("highIntensity");
 let highSliderOutput = document.getElementById("highValue");
 highSliderOutput.innerHTML = highSlider.value * 5; // Display the default slider value
-highTime = highSlider.value * 5;
 
 // Update the current slider value (each time you drag the slider handle)
 highSlider.oninput = function() {
   highSliderOutput.innerHTML = this.value * 5;
-  highTime = highSlider.value * 5;
 };
 
 let lowSlider = document.getElementById("lowIntensity");
 let lowSliderOutput = document.getElementById("lowValue");
 lowSliderOutput.innerHTML = lowSlider.value * 5; // Display the default slider value
-lowTime = lowSlider.value * 5;
 
 // Update the current slider value (each time you drag the slider handle)
 lowSlider.oninput = function() {
   lowSliderOutput.innerHTML = this.value * 5;
-  lowTime = lowSlider.value * 5;
 };
