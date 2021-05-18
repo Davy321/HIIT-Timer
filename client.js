4a9e8c28661d02f95c48dc636e71767f557235ff
Davy321 <56622066+Davy321@users.noreply.github.com>
2021-05-18 15:31:38 -0700

    Update client.js

--- client.js
+++ client.js
@@ -2,17 +2,20 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js");
 }
 
-let select = false;
+//let select = false;
 let TOption = 2; // 0 for premade, 1 for custom
 let level = 0;
-let AdvOption = false;
+let advOption = false;
+let timerFunction;
+let timerFunctionTwo;
 
 let warmupTime = 0; // time in seconds
 let highTime = 0; // high intensity training time in seconds
 let lowTime = 0; // low intensity training time in seconds
 let sets = 0; // amounts of high intensity training sets
 let currentSet = 0;
-let currentIntensity = 0; // 0 for low, 1 for high
+let currentIntensity = 1; // 0 for low, 1 for high
+let advancedIntervals = 0;
 let advancedArray = [];
 
 function selectTimerType(x) {
@@ -28,68 +31,186 @@ function selectTimerType(x) {
 function advanceTimer() {
 	if (level == 0) {
 		if (TOption == 2) {
-			document.getElementById("error").style.display = "block";
+			document.getElementById("err1").style.display = "block";
 			document.body.scrollTop = 0; // For Safari
 			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 			return;
 		}
-		document.getElementById("premade").style.display = "none";
-		document.getElementById("custom").style.display = "none";
-		document.getElementById("error").style.display = "none";
 		
-		document.getElementById("customInputParameters").style.display = "block";
-		document.getElementById("advancedSettings").style.display = "block";
-		document.getElementById("title2").innerHTML = "Basic Timer Options:";
-		document.getElementById("btn").innerHTML = "Start Timer";
-		level++;
+		
 		
 		if (TOption == 0) {
+			document.getElementById("premade").style.display = "none";
+			document.getElementById("custom").style.display = "none";
+			document.getElementById("err1").style.display = "none";
 			
+			document.getElementById("title2").innerHTML = "Premade Timers:";
 		} else {
+			document.getElementById("premade").style.display = "none";
+			document.getElementById("custom").style.display = "none";
+			document.getElementById("err1").style.display = "none";
 			
+			document.getElementById("customInputParameters").style.display = "block";
+			document.getElementById("advancedSettings").style.display = "block";
+			document.getElementById("title2").innerHTML = "Basic Timer Options:";
+			document.getElementById("advancedSettings").innerHTML = "Switch to Advanced";
+			document.getElementById("btn").innerHTML = "Start Timer";
+			
+			document.body.scrollTop = 0; // For Safari
+			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 		}//else
-	} else if (level == 1) {
-		setTimerParameters();
-    
-		document.getElementById("btn").innerHTML = "Skip";
-		document.getElementById("advancedSettings").style.display = "none";
-		document.getElementById("customInputParameters").style.display = "none";
-		document.getElementById("title2").style.display = "none";
-		
-		//warmupTime = 5;
-		//highTime = 5;
-		//lowTime = 10;
-		
-		document.getElementById("actualTimer").style.display = "block";
-		setTimeout(startInterval, warmupTime * 1000);
-		document.body.style.backgroundColor = "#a9dea0";
-		startTimer(warmupTime);
-		currentIntensity = 1;
 		
 		level++;
+		
+	} else if (level == 1) {
+		initiateWorkoutTimer();
+	} else if (level == 2){
+		clearInterval(timerFunction);
+		clearInterval(timerFunctionTwo);
+		currentSet++;
+		startAdvancedTimer();
 	} else {
-    
+		document.getElementById("actualTimer").style.display = "none";
+		document.getElementById("premade").style.outline = "none";
+		document.getElementById("custom").style.outline = "none";
+		document.getElementById("title2").innerHTML = "Choose a timer option:";
+		document.getElementById("btn").innerHTML = "Continue";
+		document.getElementById("title2").style.display = "block";
+		document.getElementById("premade").style.display = "block";
+		document.getElementById("custom").style.display = "block";
+		
+		let advElem = document.querySelectorAll(".advancedContainerInterval");
+		for (let i = 0; i < advElem.length; i++) {
+			advElem[i].remove();
+		}
+		
+		document.getElementById("advancedInputParameters").style.height = "580px";
+		document.getElementById("createInterval").style.borderTop = "none";
+		
+		TOption = 2; // 0 for premade, 1 for custom
+		level = 0;
+		advOption = false;
+		timerFunction;
+
+		warmupTime = 0; // time in seconds
+		highTime = 0; // high intensity training time in seconds
+		lowTime = 0; // low intensity training time in seconds
+		sets = 0; // amounts of high intensity training sets
+		currentSet = 0;
+		currentIntensity = 1; // 0 for low, 1 for high
+		advancedIntervals = 0;
+		advancedArray = [];
 	}
 	
 }//advanceTimer
 
 function switchSettings() {
-  if (!AdvOption) {
-	  AdvOption = true;
+  if (!advOption) {
+	  advOption = true;
 	  document.getElementById("customInputParameters").style.display = "none";
-	  document.getElementById("advancedInputParameters").style.display = "block";
-	  document.getElementById("advancedSettings").innerHTML = "Basic Options";
+	  document.getElementById("advancedSettings").innerHTML = "Switch to Basic";
 	  document.getElementById("title2").innerHTML = "Advanced Timer Options:";
+	  document.getElementById("advancedInputParameters").style.display = "block";
   } else {
-	  AdvOption = false;
-	  document.getElementById("customInputParameters").style.display = "block";
+	  advOption = false;
 	  document.getElementById("advancedInputParameters").style.display = "none";
-	  document.getElementById("advancedSettings").innerHTML = "Advanced Options";
+	  document.getElementById("advancedSettings").innerHTML = "Switch to Advanced";
 	  document.getElementById("title2").innerHTML = "Basic Timer Options:";
+	  document.getElementById("customInputParameters").style.display = "block";
   }
 }//switchSettings
 
+function initiateWorkoutTimer() {
+	if (!advOption) {
+		setTimerParameters();
+		
+		document.getElementById("title2").style.display = "none";
+		document.getElementById("btn").style.display = "none";
+		document.getElementById("customInputParameters").style.display = "none";
+		document.getElementById("advancedSettings").style.display = "none";
+		
+		//warmupTime = 5;
+		//highTime = 2;
+		//lowTime = 3;
+		
+		document.getElementById("timerTitle").innerHTML = "Warmup";
+		document.getElementById("actualTimer").style.display = "block";
+		setTimeout(startInterval, warmupTime * 1000);
+		
+		currentSet++;
+		document.body.style.backgroundColor = "#a9dea0";
+		startTimer(warmupTime);
+		
+		level++;
+	} else {
+		if (advancedIntervals == 0) {
+			document.getElementById("err2").innerHTML = "There are no intervals";
+			document.getElementById("err2").style.display = "block";
+			return;
+		}
+		
+		//push all data from intervals into an array
+		advancedArray = [];
+		
+		for (let i = 0; i < advancedIntervals; i++) {
+			let temp = document.querySelectorAll(".advName")[i].value;
+			let temp2;
+			if (temp == "") {
+				document.getElementById("err2").innerHTML = "Interval name is invalid";
+				document.getElementById("err2").style.display = "block";
+				return;
+			}
+			
+			temp = parseInt(document.querySelectorAll(".advTime")[i].value, 10);
+			temp2 = Number.isInteger(parseInt(document.querySelectorAll(".advTime")[i].value, 10));
+			if (temp > 3600 || !temp2) {
+				document.getElementById("err2").innerHTML = "Interval time is invalid";
+				document.getElementById("err2").style.display = "block";
+				return;
+			}
+			
+			let object = {
+				name:capitalizeFirstLetter(document.querySelectorAll(".advName")[i].value), 
+				time:parseInt(document.querySelectorAll(".advTime")[i].value), 
+				type:document.querySelectorAll(".advType")[i].value,
+				index:i
+			};
+			advancedArray.push(object);
+		}//for
+		
+		document.getElementById("advancedSettings").style.display = "none";
+		document.getElementById("advancedInputParameters").style.display = "none";
+		document.getElementById("title2").style.display = "none";
+		document.getElementById("err2").style.display = "none";
+		document.getElementById("btn").innerHTML = "Skip";
+		document.getElementById("actualTimer").style.display = "block";
+		level++;
+		
+		startAdvancedTimer();
+	}
+}//initiateWorkoutTimer
+
+function startAdvancedTimer() {
+	if (currentSet >= advancedIntervals) {
+		document.body.style.backgroundColor = "#d9eaf0";
+		document.getElementById("timerTitle").innerHTML = "You did it,";
+		document.getElementById("timer").innerHTML = "Congrats!";
+		document.getElementById("btn").innerHTML = "Restart";
+		level++;
+	} else {
+		document.body.style.backgroundColor = ((advancedArray[currentSet].type == "warmup") ? "#a9dea0" : (advancedArray[currentSet].type == "high") ? "#ffabab" : "#f5f2b5");
+		document.getElementById("timerTitle").innerHTML = advancedArray[currentSet].name;
+		timerFunctionTwo = setTimeout(function () {
+			
+			currentSet++;
+			startAdvancedTimer();
+		}, advancedArray[currentSet].time * 1000);
+		startTimer(advancedArray[currentSet].time);
+	}
+}//startAdvancedTimer
+
 function startInterval() {
+	currentSet++;
 	if (currentSet >= sets) {
 		startCooldown();
 		return;
@@ -101,7 +222,6 @@ function startInterval() {
 		setTimeout(function () {
 			
 			currentIntensity = 0;
-			currentSet++;
 			startInterval();
 		}, highTime * 1000);
 		startTimer(highTime);
@@ -110,7 +230,6 @@ function startInterval() {
 		document.getElementById("timerTitle").innerHTML = "Low Intensity";
 		setTimeout(function () {
 			currentIntensity = 1;
-			currentSet++;
 			startInterval();
 		}, lowTime * 1000);
 		startTimer(lowTime);
@@ -121,9 +240,10 @@ function startCooldown() {
 	document.body.style.backgroundColor = "#a9dea0";
 	document.getElementById("timerTitle").innerHTML = "Cooldown";
 	setTimeout(function () {
-      document.body.style.backgroundColor = "#d9eaf0";
+			document.body.style.backgroundColor = "#d9eaf0";
 			document.getElementById("timerTitle").innerHTML = "You did it,";
 			document.getElementById("timer").innerHTML = "Congrats!";
+			document.getElementById("btn").style.display = "block";
 			document.getElementById("btn").innerHTML = "Restart";
 		}, warmupTime * 1000);
 		startTimer(warmupTime);
@@ -139,7 +259,7 @@ function startTimer(duration) {
 	minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 	document.getElementById("timer").textContent = minutes + ":" + seconds;
-    let x = setInterval(function () {
+    timerFunction = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -150,42 +270,53 @@ function startTimer(duration) {
 
         if (--timer < 1) {
             timer = duration;
-			clearInterval(x);
+			let audio = new Audio('audio/beep.wav');
+			audio.play();
+			clearInterval(timerFunction);
         }
     }, 1000);
 }//startTimer
 
 function setTimerParameters() {
-	if (!AdvOption) {
-		warmupTime = warmupSlider.value * 30;
-		highTime = highSlider.value * 5;
-		lowTime = lowSlider.value * 5;
-		sets = (Number(document.getElementById("setAmount").value) * 2) - 1;
-	} else {
-		
-	}
+	warmupTime = warmupSlider.value * 30;
+	highTime = highSlider.value * 5;
+	lowTime = lowSlider.value * 5;
+	sets = (Number(document.getElementById("setAmount").value) * 2) + 1;
 }//getTimerParameters
 
 function createNewInterval() {
-	if (advancedArray.length == 1) {
-		let x = document.getElementById("advancedInputParameters").clientHeight + 200;
+	if (advancedIntervals > 0) {
+		let x = document.getElementById("advancedInputParameters").clientHeight + 292;
 		document.getElementById("advancedInputParameters").style.height = x + "px";
 	}
+	document.getElementById("createInterval").style.borderTop = "5px solid #7d7d7d";
 	
 	let elem = document.createElement("DIV");
 	elem.innerHTML = "<label>Name: </label>" +
-	"<input placeholder=\"e.g. Warmup\" type=\"text\" value\"sus\">" +
+	"<input class=\"advName\" type=\"text\" value=\"Interval " + (advancedIntervals + 1) + "\">" +
 	"<br><label>Duration in seconds: </label>" +
-	"<input placeholder=\"\" type=\"text\" value\"sus\">" +
+	"<input class=\"advTime\" type=\"text\">" +
 	"<br><label>Interval Type: </label>" +
-	"<select><option value=\"Warmup\">Warmup/Cooldown</option>" + 
-	"<option value=\"High\">High Intensity</option>" +
-	"<option value=\"Low\">Low Intensity</option></select>"
+	"<select class=\"advType\"><option value=\"warmup\">Warmup/Cooldown</option>" + 
+	"<option value=\"high\">High Intensity</option>" +
+	"<option value=\"low\">Low Intensity</option></select>" +
+	"<br><button onclick=\"deleteInterval(" + advancedIntervals +")\">Delete Interval</button>"
 	;
-	document.getElementById("advancedInputParameters").appendChild(elem);
+	document.getElementById("advancedInputParameters").insertBefore(elem, document.getElementById("createInterval"));
 	elem.className = "advancedContainerInterval";
-	advancedArray.push(elem);
+	elem.setAttribute("id", "interval" + advancedIntervals);
+	advancedIntervals++;
 	
+}
+
+function deleteInterval(x) {
+	document.getElementById("interval" + x).remove();
+	advancedIntervals--;
+	
+	if (advancedIntervals > 0) {
+		let x = document.getElementById("advancedInputParameters").clientHeight - 292;
+		document.getElementById("advancedInputParameters").style.height = x + "px";
+	}
 }
 
 let warmupSlider = document.getElementById("warmup");
@@ -214,3 +345,104 @@ lowSliderOutput.innerHTML = lowSlider.value * 5; // Display the default slider v
 lowSlider.oninput = function() {
   lowSliderOutput.innerHTML = this.value * 5;
 };
+
+function downloadTimer() {
+	advancedArray = [];
+	
+	for (let i = 0; i < advancedIntervals; i++) {
+		let object = {
+			name:capitalizeFirstLetter(document.querySelectorAll(".advName")[i].value), 
+			time:parseInt(document.querySelectorAll(".advTime")[i].value), 
+			type:document.querySelectorAll(".advType")[i].value,
+			index:i
+		};
+		advancedArray.push(object);
+	}//for
+	
+	download("customTimer.txt", JSON.stringify(advancedArray));
+}
+
+//do stuff with advanced options file upload
+let fileInput = document.getElementById("fileUpload");
+fileInput.oninput = function() {
+	let file = fileInput.files[0];
+	let fileText;
+	
+	// new FileReader object
+	let reader = new FileReader();
+
+	// event fired when file reading finished
+	reader.addEventListener('load', function(e) {
+	   // contents of the file
+	    fileText = e.target.result;
+		displayImportedTimer(fileText);
+		fileInput.value = "";
+	});
+
+	// event fired when file reading failed
+	reader.addEventListener('error', function() {
+	    alert('Error : Failed to read file');
+	});
+
+	// read file as text file
+	reader.readAsText(file);
+};
+
+function displayImportedTimer(file) {
+	advancedIntervals = 0;
+	advancedArray = [];
+	
+	document.getElementById("advancedInputParameters").style.height = "580px";
+	let advElem = document.querySelectorAll(".advancedContainerInterval");
+	for (let i = 0; i < advElem.length; i++) {
+		advElem[i].remove();
+	}
+	
+	advancedArray = JSON.parse(file);
+	
+	for (let i = 0; i < advancedArray.length; i++) {
+		if (advancedIntervals > 0) {
+			let x = document.getElementById("advancedInputParameters").clientHeight + 292;
+			document.getElementById("advancedInputParameters").style.height = x + "px";
+		}
+		document.getElementById("createInterval").style.borderTop = "5px solid #7d7d7d";
+		
+		let elem = document.createElement("DIV");
+		elem.innerHTML = "<label>Name: </label>" +
+		"<input class=\"advName\" type=\"text\" value=\"" + advancedArray[i].name + "\">" +
+		"<br><label>Duration in seconds: </label>" +
+		"<input class=\"advTime\" type=\"text\" value=\"" + advancedArray[i].time + "\">" +
+		"<br><label>Interval Type: </label>" +
+		"<select class=\"advType\"><option value=\"warmup\">Warmup/Cooldown</option>" + 
+		"<option value=\"high\">High Intensity</option>" +
+		"<option value=\"low\">Low Intensity</option></select>" +
+		"<br><button onclick=\"deleteInterval(" + advancedIntervals +")\">Delete Interval</button>"
+		;
+		document.getElementById("advancedInputParameters").insertBefore(elem, document.getElementById("createInterval"));
+		elem.className = "advancedContainerInterval";
+		elem.setAttribute("id", "interval" + advancedIntervals);
+		advancedIntervals++;
+	}//for
+	
+	let elems = document.querySelectorAll(".advType");
+	for (let j = 0; j < advancedArray.length; j++) {
+		elems[j].value = advancedArray[j].type;
+	}//for
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}//capitalizeFirstLetter
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}//download
